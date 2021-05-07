1eb65dc703cc8333eb2c9be347229d4b506eaf2d
Tomás Alves <tomasalves35@gmail.com>
2021-05-07 14:42:08 +0100

    Design login

--- src/atlanticwarfare/design/FormLogin.java
+++ src/atlanticwarfare/design/FormLogin.java
@@ -1,72 +1,108 @@
 package atlanticwarfare.design;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.IOException;
 
-import javax.swing.BorderFactory;
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-import javax.swing.JPasswordField;
-import javax.swing.JTextField;
+import javax.swing.JRadioButton;
 
 import atlanticwarfare.database.Player;
+import atlanticwarfare.utilities.HintPasswordField;
+import atlanticwarfare.utilities.HintTextField;
 
 public class FormLogin extends JFrame implements ActionListener{
 	private static final long serialVersionUID = 1L;
-	private JTextField tfNomeUtilizador;
-	private JPasswordField tfPalavraChave;
+	private HintTextField tfNomeUtilizador;
+	private HintPasswordField tfPalavraChave;
+	private JLabel iconUser, iconPass;
 
 	public FormLogin() {
 		super();
 
-		setSize(300,200);
-		getContentPane().setLayout(null);
-		setLocationRelativeTo(null);
+		setSize(400 ,400);
 		setResizable(false);
-		criarTexto();
+		getContentPane().setLayout(null);
+		
+		criarFundo();
 		criarCaixaTexto();
 		criarBotoes();
+		criarImagens();
+		
+		addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				requestFocus(); 
+			}
+		});
 
+		setLocationRelativeTo(null);
 		setVisible(true);
 	}
 
-	public void criarTexto() {
-		JLabel lblNomeUtilizador = new JLabel("Nome de Utilizador:");
-		lblNomeUtilizador.setBounds(10, 11, 124, 14);
-		getContentPane().add(lblNomeUtilizador);
-
-		JLabel lblPalavraChave = new JLabel("Palavra Chave:");
-		lblPalavraChave.setBounds(10, 53, 124, 14);
-		getContentPane().add(lblPalavraChave);
+	public void criarFundo(){
+		try {
+			setContentPane(new JLabel(new ImageIcon(ImageIO.read(new File("Images/Fundo.png")))));
+			setBounds(0, 0, getWidth(), getHeight());
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		pack();
 	}
 
 	public void criarCaixaTexto() {
-		tfNomeUtilizador = new JTextField();
-		tfNomeUtilizador.setBounds(160, 8, 86, 20);
-		getContentPane().add(tfNomeUtilizador);
-		tfNomeUtilizador.setColumns(10);
+		tfNomeUtilizador = new HintTextField(" Nome de Utilizador");
+		tfNomeUtilizador.setBounds(100, 200, 200, 20);
+		tfNomeUtilizador.setBorder(javax.swing.BorderFactory.createEmptyBorder());
+		getContentPane().add(tfNomeUtilizador);		
+		tfNomeUtilizador.setColumns(10);	
 
-		tfPalavraChave = new JPasswordField();
-		tfPalavraChave.setBounds(160, 50, 86, 20);
+		tfPalavraChave = new HintPasswordField(" Palavra Chave");
+		tfPalavraChave.setBounds(100, 250, 200, 20);
+		tfPalavraChave.setBorder(javax.swing.BorderFactory.createEmptyBorder());
 		getContentPane().add(tfPalavraChave);
 		tfPalavraChave.setColumns(10);
 	}
 
 	public void criarBotoes() {
-		JButton btnLogin = new JButton("Login", new ImageIcon("Images/login.png"));
-		btnLogin.setBounds(12,getHeight()-100,125,43);
+		JRadioButton btn = new JRadioButton("text");
+		getContentPane().add(btn);
+		
+		JButton btnLogin = new JButton("Login");
+		btnLogin.setBounds(35,325,150,25);
 		btnLogin.addActionListener(this);
+		btnLogin.setBorder(null);
+		btnLogin.setBackground(Color.WHITE);
 		btnLogin.setActionCommand("Login");
 		getContentPane().add(btnLogin);
 
-		JButton btnRegister = new JButton("Registrar", new ImageIcon("Images/register.png"));
-		btnRegister.setBounds(btnLogin.getBounds().x + 135,btnLogin.getBounds().y,125,43);
+		JButton btnRegister = new JButton("Registar");
+		btnRegister.setBounds(215,325,150,25);
 		btnRegister.addActionListener(this);
+		btnRegister.setFocusable(false);
+		btnRegister.setBorder(null);
+		btnRegister.setBackground(Color.WHITE);
 		btnRegister.setActionCommand("Registar");
 		getContentPane().add(btnRegister);
+	}
+	
+	public void criarImagens(){
+		iconUser = new JLabel(new ImageIcon(System.getProperty("user.dir") + "//Images//nomeUtilizador.png"));
+		iconUser.setBounds(80, 201, 16, 16);
+		getContentPane().add(iconUser);
+		
+		iconPass = new JLabel(new ImageIcon(System.getProperty("user.dir") + "//Images//pass.png"));
+		iconPass.setBounds(80, 251, 16, 16);
+		getContentPane().add(iconPass);
 	}
 
 	@Override
@@ -81,15 +117,15 @@ public class FormLogin extends JFrame implements ActionListener{
 					new Game(player);
 					setVisible(false);
 				}else {
-					JOptionPane.showMessageDialog(new JFrame(), "Utilizador não encontrado!", "ERRO",
-					        JOptionPane.ERROR_MESSAGE);
+					JOptionPane.showMessageDialog(new JFrame(), "Utilizador nï¿½o encontrado!", "ERRO",
+							JOptionPane.ERROR_MESSAGE);
 				}	
 			}else {
 				JOptionPane.showMessageDialog(new JFrame(), "Precisa de preencher todos os campos!", "ERRO",
-				        JOptionPane.ERROR_MESSAGE);
+						JOptionPane.ERROR_MESSAGE);
 			}
-			
 		}
+		
 		if (ae.getActionCommand().equals("Registar")) {
 			new FormRegister();
 		}
